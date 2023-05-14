ed9570075eecaa51cc69fef75925313d08bee789
Marco Martinez <martinezma025@gmail.com>
2023-05-14 16:45:17 -0500

    added more routes. currently on chapter 2.4

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(response http.ResponseWriter, request *http.Request) {
+
+	if request.URL.Path != "/" {
+		http.NotFound(response, request)
+		return
+	}
+
+	response.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(response http.ResponseWriter, request *http.Request) {
+	response.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(response http.ResponseWriter, request *http.Request) {
+	response.Write([]byte("Create a new snippet..."))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Print("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
