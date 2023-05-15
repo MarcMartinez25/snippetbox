318ff6f9736172b0ad85c6f6cf4fd56b2a435deb
Marco Martinez <martinezma025@gmail.com>
2023-05-14 19:36:34 -0500

    chapters 2.4-2.5 : customized the http headers and added url query string to the snippet view handler

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(response http.ResponseWriter, request *http.Request) {
@@ -16,10 +18,24 @@ func home(response http.ResponseWriter, request *http.Request) {
 }
 
 func snippetView(response http.ResponseWriter, request *http.Request) {
-	response.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(request.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(response, request)
+		return
+	}
+
+	fmt.Fprintf(response, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreate(response http.ResponseWriter, request *http.Request) {
+
+	if request.Method != "POST" {
+		response.Header().Set("Allow", http.MethodPost)
+		http.Error(response, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	response.Write([]byte("Create a new snippet..."))
 }
 
