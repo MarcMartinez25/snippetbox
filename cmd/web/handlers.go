dc78c0af2297d73d7497f53897cfbeed5e05803f
Marco Martinez <martinezma025@gmail.com>
2023-05-14 19:49:00 -0500

    Chapter 2.6 : restructured the project

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(response http.ResponseWriter, request *http.Request) {
-
 	if request.URL.Path != "/" {
 		http.NotFound(response, request)
 		return
@@ -29,7 +27,6 @@ func snippetView(response http.ResponseWriter, request *http.Request) {
 }
 
 func snippetCreate(response http.ResponseWriter, request *http.Request) {
-
 	if request.Method != "POST" {
 		response.Header().Set("Allow", http.MethodPost)
 		http.Error(response, "Method Not Allowed", http.StatusMethodNotAllowed)
@@ -37,15 +34,4 @@ func snippetCreate(response http.ResponseWriter, request *http.Request) {
 	}
 
 	response.Write([]byte("Create a new snippet..."))
-}
-
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
 }
